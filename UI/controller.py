325b770405609e01b7a7ce0c9a600bfcf105b48c
Lorenzo Luison <s325607@studenti.polito.it>
2026-05-18 16:00:46 +0200

    esame con comp connessa finito

--- UI/controller.py
+++ UI/controller.py
@@ -9,8 +9,82 @@ class Controller:
         self._model = model
 
     def handleAnalizzaOggetti(self, e):
-        pass
+        self._model.buildGraph()
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text("Grafo correttamente creato."))
+        self._view.txt_result.controls.append(
+            ft.Text(f"Il grafo contiene {self._model.getNumNodes()} nodi e {self._model.getNumEdges()} archi."))
+
+        self._view._txtIdOggetto.disabled = False
+        self._view._btnCompConnessa.disabled = False
+        self._view.update_page()
 
     def handleCompConnessa(self,e):
-        pass
+        txtIdOggetto = self._view._txtIdOggetto.value
 
+        if txtIdOggetto == "":
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text(f"Attenzione, inserire un valore nel campo id.", color="red"))
+            self._view.update_page()
+            return
+
+        try:
+            idOggetto = int(txtIdOggetto)
+        except ValueError:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text(f"Attenzione, inserire un valore numerico nel campo id.", color="red"))
+            self._view.update_page()
+            return
+
+        if not self._model.hasNode(idOggetto):
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text(f"Attenzione, l'id inserito non è presente nel grafo.", color="orange"))
+            self._view.update_page()
+            return
+
+        sizeCompConn = self._model.getInfoCompConnessa(idOggetto)
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+            ft.Text(f"La componente connessa contenente l'oggetto con id {idOggetto} è composta di {sizeCompConn} nodi.", color="green"))
+
+        self._view._ddLun.disabled = False
+        self._view._btnCerca.disabled = False
+
+        lunValues = list(range(2, sizeCompConn))
+
+        # for v in lunValues:
+        #     self._view._ddLun.options.append(ft.dropdown.Option(v))
+
+        lunValuesDD = list(map(lambda x: ft.dropdown.Option(x), lunValues))
+
+        self._view._ddLun.options = lunValuesDD
+
+        self._view.update_page()
+
+
+    def handleCerca(self, e):
+        source = self._model.getNodeFromId(int(self._view._txtIdOggetto.value))
+
+        lun = self._view._ddLun.value
+
+        if lun is None:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(
+                ft.Text("Attenzione, selezionare un"
+                        "valore di lunghezza fra le scelte proposte.", color="red"))
+            self._view.update_page()
+            return
+
+        lunInt = int(lun)
+
+        path, cost = self._model.getOptPath(source, lunInt)
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+                ft.Text(f"Ho trovato un cammino che parte da {source} "
+                        f"che ha un peso totale pari a {cost}.", color= "green"))
+        self._view.txt_result.controls.append(
+                ft.Text(f"Di seguito i nodi che compongono questo cammino:", color= "green"))
+        for p in path:
+            self._view.txt_result.controls.append(ft.Text(p))
+
+        self._view.update_page()
--- model/model.py
+++ model/model.py
@@ -1,3 +1,104 @@
+import copy
+
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._graph = nx.Graph()
+        self._nodes = DAO.getAllNodes()
+        self._idMapAO = {}
+        for n in self._nodes:
+            self._idMapAO[n.object_id] = n
+        self._optPath = []
+        self._optCost = 0
+
+    def getOptPath(self, source, lun):
+        parziale = [source]
+
+        for n in self._graph.neighbors(source):
+            if n.classification == parziale[-1].classification:
+                parziale.append(n)
+                self._ricorsione(parziale, lun)
+                parziale.pop() # Backtracking
+        return self._optPath, self._optCost
+
+    def _ricorsione(self, parziale, lun):
+        if len(parziale) == lun:
+            # condizione di terminazione, allora parziale è lunga esattamente lun
+            # per cui verifico che questa parziale sia meglio del mio best
+            # (condizione di ottimalità), ed in ogni caso esco.
+            if self._costoPath(parziale) > self._optCost:
+                self._optCost = self._costoPath(parziale)
+                self._optPath = copy.deepcopy(parziale)
+            return
+
+        #se arrivo qui, posso ancora aggiungere nodi
+        for n in self._graph.neighbors(parziale[-1]):
+            if parziale[-1].classification == n.classification:
+                parziale.append(n)
+                self._ricorsione(parziale, lun)
+                parziale.pop() # Backtracking
+
+    def _costoPath(self, path):
+        costo = 0
+        for i in range(0, len(path)-1):
+            costo += self._graph[path[i]][path[i+1]]["weight"]
+
+        return costo
+
+    def getInfoCompConnessa(self, id_oggetto):
+        #cercare la componente connessa che contiene id_oggetto
+
+        if not self.hasNode(id_oggetto):
+            return None
+
+        source = self._idMapAO[id_oggetto]
+
+        #Strategia 1
+        dfsTree = nx.dfs_tree(self._graph, source)
+        print("size connessa con dfs_tree", len(dfsTree.nodes()))
+
+        #Strategia 2
+        dfsPred = nx.dfs_predecessors(self._graph, source)
+        print("size connessa con dfs_predecessors", len(dfsPred.values()))
+
+        #Strategia 3
+        conn = nx.node_connected_component(self._graph, source)
+        print("size connessa con node_connected_component", len(conn))
+
+        return len(conn)
+
+    def hasNode(self, id_oggetto):
+        return id_oggetto in self._idMapAO
+
+    def getNodeFromId(self, id_oggetto):
+        return self._idMapAO[id_oggetto]
+
+    def buildGraph(self):
+        #aggiunge i nodi
+        self._graph.add_nodes_from(self._nodes)
+
+        #aggiunge gli archi
+        self.addEdgesV2()
+
+    def addEdges(self):
+        for u in self._nodes:
+            for v in self._nodes:
+                peso = DAO.getEdgePeso(u, v)
+                if peso is not None:
+                    self._graph.add_edge(u, v, weight = peso)
+                    print(f"Aggiunto arco fra {u} e {v} con peso {peso}")
+
+    def addEdgesV2(self):
+        allEdges = DAO.getAllEdges(self._idMapAO)
+        for e in allEdges:
+            self._graph.add_edge(e.o1, e.o2, weight = e.peso)
+
+    def getNumNodes(self):
+        return len(self._graph.nodes)
+
+    def getNumEdges(self):
+        return len(self._graph.edges)
